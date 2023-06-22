1949e52d675e7501fde2eab348dc0c611c32691b
axs1485 <axs1485.student.bham.ac.uk>
2023-06-23 00:06:46 +0100

    Created models

---
+++ models.py
@@ -0,0 +1,28 @@
+import enum
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Type(enum.Enum):
+    question = "Question"
+    response = "Response"
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True, index=True)
+    sibling_id=Column(Integer, ForeignKey('Message.id'))
+    message_type = Column(Type)
+    message_body= Column(String)
+    assigned_codes=Column(Integer, ForeignKey('code.id'))
+
+class Code(Base):
+    __tablename__ = 'code'
+    id = Column(Integer, primary_key=True, index=True)
+    code_body = Column(String)
+
+class AssignedCodes(Base):
+    __tablename__ = 'assigned codes'
+    message_id = Column(Integer, ForeignKey('Message.id'))
+    code_id = Column(Integer, ForeignKey('Code.id'))
